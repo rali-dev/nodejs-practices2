f68af7ae44bd5303cdd6c064f7bce102ccc030e6
rahimi.ali <ali.saee@outlook.de>
2025-09-16 23:15:05 +0200

    7.nodejs - GET( fetching one user )

--- appCRUD.js
+++ appCRUD.js
@@ -13,5 +13,19 @@ app.get('/api/users', (req, res) =>{
   });
 });
 
+//http://localhost:3000/api/users/3
+//http://localhost:3000/api/users/23
+app.get('/api/users/:id', (req, res)=>{
+   const user = users.find(u => u.id === parseInt(req.params.id)); 
+  //  if(!user) return res.status(404).send('The user with the given ID was not found.');
+    if(!user) return res.status(404).json({
+      error: 'The user with the given ID was not found.'
+   });
+   res.json({
+      data: user, 
+      message: 'ok'
+   });
+})
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
