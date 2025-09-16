631ef315ce438166647546c42493731008e1437d
rahimi.ali <ali.saee@outlook.de>
2025-09-16 22:03:48 +0200

    4.nodejs - GET( sending data via URL Parameters and using req.params )

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ app.get('/api/users', (req, res) => {
     {id: 3, name: 'user3'}
   ]);
 });
+
+app.get('/api/users/:id', (req,res) => {
+  console.log(req.params.id);
+  const user = {id: req.params.id, name: `user${req.params.id}`};
+  res.send(user);
+});
+
+// Use the PORT environment variable or default to 3000
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 //$env:PORT=1313
