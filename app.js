046b76f165983cca620dc13977b96ab71b47b2d6
rahimi.ali <ali.saee@outlook.de>
2025-09-16 19:00:24 +0200

    2.nodejs - nodemon

--- app.js
+++ app.js
@@ -5,4 +5,12 @@ app.get('/', (req, res) => {
   res.send('Hello Express!');
 });
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+app.get('/api/users', (req, res) => {
+  res.send([
+    {id: 1, name: 'user1'},
+    {id: 2, name: 'user2'},
+    {id: 3, name: 'user3'}
+  ]);
+});
+
+app.listen(3000, () => console.log('Listening on port 3000' ));
